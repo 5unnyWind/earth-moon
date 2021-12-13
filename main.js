960f38df143d277449a88c94a6bae9cd0f65f14a
sunnyWind <1749790593@qq.com>
2021-12-13 10:08:32 +0800

    initialization

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 import './style.css'
 import * as THREE from 'three'
-import earthMap from './textures/planets/earth_atmos_2048.jpg'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer'
+
+import earthMap from './textures/planets/earth_atmos_2048.jpg'
+import moonMap from './textures/planets/moon_1024.jpg'
+
 
 const earth_radius = 2.5
 const moon_radius = 0.27
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 200)
-camera.position.x = 30
-camera.position.y = 30
-camera.position.z = 40
+camera.position.x = 5
+camera.position.y = 5
+camera.position.z = 20
 camera.lookAt(scene.position)
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
 
 const axes = new THREE.AxesHelper(30)
 scene.add(axes)
 
-const controls = new OrbitControls(camera,renderer.domElement)
-
 
 const spotlight = new THREE.SpotLight(0xffffff)
-spotlight.position.set(30,20,0)
+spotlight.position.set(30, 20, 0)
 scene.add(spotlight)
 
 const spotLightHelper = new THREE.SpotLightHelper(spotlight)
@@ -41,17 +40,29 @@ const earthM = new THREE.MeshPhongMaterial({ color: 0xfffff, map: textureLoader.
 const earth = new THREE.Mesh(earthG, earthM)
 scene.add(earth)
 
+const mooonG = new THREE.SphereGeometry(moon_radius, 20, 20)
+const moonM = new THREE.MeshPhongMaterial({ map: textureLoader.load(moonMap) })
+const moon = new THREE.Mesh(mooonG, moonM)
+moon.position.set(-5, 0, 0)
+scene.add(moon)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setPixelRatio(devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
 renderer.render(scene, camera)
-controls.update()
-function animate() {
 
-	requestAnimationFrame( animate );
+const clock = new THREE.Clock()
 
-	// required if controls.enableDamping or controls.autoRotate are set to true
+
+const animate = () => {
+	const elapsed = clock.getElapsedTime()
+	moon.position.set(5 * Math.sin(elapsed), 0, 5 * Math.cos(elapsed))
+	earth.rotation.y = elapsed
 	controls.update();
-
-	renderer.render( scene, camera );
-
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
 }
 animate()
 // document.querySelector('#app').innerHTML = `
